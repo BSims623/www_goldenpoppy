4f2948fff50da354aaa235d53d3f95c22b07abcf
Brian Sims <briansims623@gmail.com>
2025-01-03 12:16:42 -0800

    Added photo to Get A Free Quote Page

--- src/app/free-quote/page.tsx
+++ src/app/free-quote/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { FreeQuoteForm } from "./_components/FreeQuoteForm";
+import ExportedImage from "next-image-export-optimizer";
 
 export const metadata: Metadata = {
   title: "Free Quote",
@@ -11,7 +12,15 @@ export default function FreeQuotePage() {
   return (
     <main className="min-h-mainSize bg-secondary px-4 py-10 text-primary lg:py-20">
       <section className="mx-auto w-full max-w-5xl">
-        <h1 className="mb-4 text-4xl">
+        <ExportedImage
+          className="object-cover"
+          src="/images/community.webp"
+          alt="An image of a community of homes."
+          width={1024}
+          height={800}
+          priority
+        />
+        <h1 className="mb-4 pt-10 text-4xl">
           Fill out the form below for a free quote.
         </h1>
         <FreeQuoteForm />
